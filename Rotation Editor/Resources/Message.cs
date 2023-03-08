c3b20805f46a57a82fffa0650897424907196d62
Botond Lajos Perényi <halkszavu@outlook.com>
2023-03-08 23:22:05 +0100

    Message added

---
+++ Rotation Editor/Resources/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotation_Editor
+{
+	internal class Message : INotifyPropertyChanged
+	{
+
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+		private void OnPropertyChanged([CallerMemberName] string property = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+	}
+}
