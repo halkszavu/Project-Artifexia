2c0bb8bac4aa6eb89e959b7446324d854b4a81a6
Botond Lajos Perényi <halkszavu@outlook.com>
2023-03-09 13:48:41 +0100

    Is a particular ID is in use

--- Rotation Editor/ViewModel/RotationModel.cs
+++ Rotation Editor/ViewModel/RotationModel.cs
@@ -67,11 +67,11 @@ namespace Rotation_Editor.ViewModel
 
 		public (double latitude, double longitude, double angle) GetCoordinatesOfIDAtTimestep(int plateId, double timeStamp, bool isUpper = true)
 		{
-			var plateRotations = Rotations.Where(r => r.PlateID == plateId).Where(r =>r.TimeStamp == timeStamp).ToList();
+			var plateRotations = Rotations.Where(r => r.PlateID == plateId).Where(r => r.TimeStamp == timeStamp).ToList();
 			if (plateRotations.Any())
 			{
-				if(plateRotations.Count == 1)
-					return ( plateRotations[0].Latitude, plateRotations[0].Longitude, plateRotations[0].Angle);
+				if (plateRotations.Count == 1)
+					return (plateRotations[0].Latitude, plateRotations[0].Longitude, plateRotations[0].Angle);
 				else
 				{
 					if (isUpper)
@@ -84,7 +84,9 @@ namespace Rotation_Editor.ViewModel
 				throw new ArgumentException("There is no Coordinates for this ID at this Timestamp");
 		}
 
+		public bool IsIDInUse(int id) => PlateIds.Contains(id);
+
 		public event PropertyChangedEventHandler? PropertyChanged;
-		private void OnPropertyChanged([CallerMemberName] string  propertyName = "")=>PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		private void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 }
