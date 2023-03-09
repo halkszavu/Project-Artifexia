7c291aa5796ac9c4dd944244201451929e106dc6
Botond Lajos Perényi <halkszavu@outlook.com>
2023-03-09 19:38:21 +0100

    Creating all the necessary window changes

--- Rotation Editor/Forms/Coordinate.xaml.cs
+++ Rotation Editor/Forms/Coordinate.xaml.cs
@@ -19,6 +19,45 @@ namespace Rotation_Editor
 	/// </summary>
 	public partial class Coordinate : BaseWindow
 	{
+		double _latitude;
+		public double Latitude
+		{
+			get => _latitude;
+			set
+			{
+				if (_latitude != value)
+				{
+					_latitude = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+		double _longitude;
+		public double Longitude
+		{
+			get => _longitude;
+			set
+			{
+				if(value != _longitude)
+				{
+					_longitude = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+		double _angle;
+		public double Angle
+		{
+			get => _angle;
+			set
+			{
+				if(value != _angle)
+				{
+					_angle = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 		public Coordinate()
 		{
 			InitializeComponent();
--- Rotation Editor/Forms/TimeStamp.xaml.cs
+++ Rotation Editor/Forms/TimeStamp.xaml.cs
@@ -19,6 +19,20 @@ namespace Rotation_Editor
 	/// </summary>
 	public partial class TimeStamp : BaseWindow
 	{
+		double _timeStamp;
+		public double DesiredTimestamp
+		{
+			get => _timeStamp;
+			set
+			{
+				if(_timeStamp != value)
+				{
+					_timeStamp = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
 		public TimeStamp()
 		{
 			InitializeComponent();
