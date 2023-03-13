b9e8bb6797eedd9e33c451fd0dfdd7599d8b0174
Botond Lajos Perényi <halkszavu@outlook.com>
2023-03-13 09:49:34 +0100

    TwoPlateID added, and working.

--- Rotation Editor/Forms/TwoPlateID.xaml.cs
+++ Rotation Editor/Forms/TwoPlateID.xaml.cs
@@ -12,13 +12,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Rotation_Editor.Forms
+namespace Rotation_Editor
 {
 	/// <summary>
 	/// Interaction logic for TwoPlateID.xaml
 	/// </summary>
-	public partial class TwoPlateID : Window
+	public partial class TwoPlateID : BaseWindow
 	{
+		public List<int> PlateIDs
+		{
+			get => ParentWnd.Model.GetPlateIDs;
+		}
+
+		int _1selected;
+		public int FirstSelectedIndex
+		{
+			get => _1selected;
+			set
+			{
+				if (_1selected != value)
+				{
+					_1selected = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+		int _2selected;
+		public int SecondSelectedIndex
+		{
+			get => _2selected;
+			set
+			{
+				if (_2selected != value)
+				{
+					_2selected = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 		public TwoPlateID()
 		{
 			InitializeComponent();
