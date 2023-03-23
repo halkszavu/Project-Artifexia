700ad7a743aa4ab481e08868c4155d70313c7b59
Botond Lajos Perényi <halkszavu@outlook.com>
2023-03-23 09:17:20 +0100

    Button Commands added

--- Rotation Editor/Resources/CustomCommands.cs
+++ Rotation Editor/Resources/CustomCommands.cs
@@ -19,14 +19,8 @@ namespace RotationEditor.Resources
 			);
 
 		public static readonly RoutedUICommand Testing = new RoutedUICommand("Testing", "Testing", typeof(CustomCommands));
-
-		public static readonly RoutedUICommand SaveAs = new RoutedUICommand("Save as", "SaveAs", typeof(CustomCommands),
-			new InputGestureCollection()
-				{
-					new KeyGesture(Key.S, ModifierKeys.Control | ModifierKeys.Shift),
-				}
-			);
-
-		
+		public static readonly RoutedCommand Comment = new RoutedUICommand("Comment", "Comment", typeof(CustomCommands));
+		public static readonly RoutedCommand Add = new RoutedUICommand("Add", "Add", typeof(CustomCommands));
+		public static readonly RoutedCommand Move = new RoutedUICommand("Move", "Move", typeof(CustomCommands));
 	}
 }
