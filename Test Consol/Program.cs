64eba5e31baff941588447d5e1c83ef8dd1ae5b2
Botond Lajos Perényi <halkszavu@outlook.com>
2023-04-02 15:47:16 +0200

    Testing no longer works.

--- Test Consol/Program.cs
+++ Test Consol/Program.cs
@@ -4,6 +4,4 @@ using Test_Consol;
 using static System.Console;
 
 WriteLine("Initializing Test Console");
-WriteLine("Parsing the second rotation test case:");
-RotationModel.RotationRecontstructionModel reconstruction = Parser.ParseReconstruction(TestCases.Test2);
 ReadKey();
