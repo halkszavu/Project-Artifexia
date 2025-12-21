556fd8a2c5214ba4fb465610fda30453f0bbedd8
halkszavu <halkszavu@outlook.com>
2025-12-21 20:57:48 +0100

    cratonCreator init

---
+++ Rotation Editor/Views/CratonCreation.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Rotation_Editor.Views
+{
+	/// <summary>
+	/// Interaction logic for CratonCreation.xaml
+	/// </summary>
+	public partial class CratonCreation : Window
+	{
+		public CratonCreation()
+		{
+			InitializeComponent();
+		}
+	}
+}
